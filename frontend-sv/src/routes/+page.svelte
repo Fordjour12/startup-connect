619970ee36325ee2b2107145d8d949734e9ddce1
McCamble Kofi Fordjour Bobie <fordjourbobiem@gmail.com>
2025-07-18 18:39:58 +0000

    fixing service name mistakes

--- frontend-sv/src/routes/+page.svelte
+++ frontend-sv/src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-    Welcome to the application frontend <strong>startup-connect</strong>
+    Welcome to the application frontend <strong>startup-connect</strong>.
 </h1>
